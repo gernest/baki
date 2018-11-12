6684e51668d99d8532c892e86f39416f8b41d33f
gernest <geofreyernest@live.com>
2018-11-12 10:37:19 +0300

    start work on lexer

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const mem = std.mem;
+const utf8 = @import("./zunicode/src/utf8/index.zig");
 
 const space: i32 = 0x20;
 const tab: i32 = 0x09;
@@ -11,8 +13,8 @@ pub const Markdown = struct.{
     ///Token stores details about the markdown token.
     pub const Token = struct.{
         id: Id,
-        begin: u64,
-        end: u64,
+        begin: usize,
+        end: usize,
     };
 
     pub const Item = struct.{
@@ -29,12 +31,14 @@ pub const Markdown = struct.{
 
     /// is a list of collected tokens. This is null if parse hasn't been called
     /// yet.
-    tokens: ?std.ArrayList(Token),
+    tokens: ?ToeknList,
+    allocator: *mem.Allocator,
+
+    pub const ToeknList = std.ArrayList(Token);
 
     pub const Id = enum.{
         EOF,
         Illegal, // refrered as insecure characters
-        HashTag, // #
         Space,
         Tab,
         NewLine,
@@ -46,5 +50,37 @@ pub const Markdown = struct.{
 
     // lex braks down src into smaller tokens. This interprets src as unicode
     // code points.
-    fn lex(self: *Markdown, src: []const u8) !void {}
+    fn lex(self: *Markdown, src: []const u8) !void {
+        var iterator = utf8.Iterator.init(src);
+        if (self.tokens != null) {
+            // just clear the prefious tokens
+            self.tokens.?.resize(0);
+        } else {
+            self.tokens = ToeknList.init(self.allocator);
+        }
+
+        var last_pos: usize = 0;
+        while (true) {
+            var token: Token = undefined;
+            token.begin = last_pos;
+            const rune = try iterator.next();
+            if (rune == null) {
+                token.id = Id.EOF;
+                self.tokens.?.append(token);
+                break;
+            }
+            token.end = last_pos + rune.size;
+            switch (rune.value) {
+                0x00 => token.id = Id.Illegal,
+                space => token.id = Id.Space,
+                tab => token.id = Id.Tab,
+                new_line => token.id = Id.NewLine,
+                carriage_return => token.id = Id.CarriageReturn,
+                line_tabulation => token.id = Id.LineTabulation,
+                form_feed => token.id = Id.FormFeed,
+                else => unreachable,
+            }
+            self.tokens.?.append(token);
+        }
+    }
 };
