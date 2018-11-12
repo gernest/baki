93cf31fcdc7c93b4689e9cc62963a09bf682902d
gernest <geofreyernest@live.com>
2018-11-12 09:32:06 +0300

    initial API definition

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,36 @@
 const std = @import("std");
-const assertOrPanic = std.debug.assertOrPanic;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+pub const Markdown = struct.{
+    ///Token stores details about the markdown token.
+    pub const Token = struct.{
+        id: Id,
+        begin: u64,
+        end: u64,
+    };
 
-test "basic add functionality" {
-    assertOrPanic(add(3, 7) == 10);
-}
+    pub const Item = struct.{
+        token: Token,
+        kind: ItemKind,
+    };
+
+    /// ItemKind defines possible kinds of structures that can be represented by
+    /// a markdown document.
+    pub const ItemKind = enum.{
+        Block,
+        Inline,
+    };
+
+    /// is a list of collected tokens. This is null if parse hasn't been called
+    /// yet.
+    tokens: ?std.ArrayList(Token),
+
+    pub const Id = enum.{
+        EOF,
+        HashTag, // #
+        Escape, // #
+    };
+
+    // lex braks down src into smaller tokens. This interprets src as unicode
+    // code points.
+    fn lex(self: *Markdown, src: []const u8) !void {}
+};
