a427a489f2ee82bbf9abdb72d078caea2986c7d8
gernest <geofreyernest@live.com>
2019-01-01 22:32:36 +0300

    update texlexer

--- src/baki.zig
+++ src/baki.zig
@@ -208,13 +208,32 @@ const Lexer = struct {
 
     const TextLexer = struct {
         state: lexState,
+
         fn init() TextLexer {
             return TextLexer{
                 .state = lexState{ .lexFn = lexFn },
             };
         }
+
         fn lexFn(self: *lexState, lx: *Lexer) !?*lexState {
-            return error.TODO;
+            while (try lx.peek()) |r| {
+                switch (r) {
+                    '\n' => {
+                        if (ls.current_pos > ls.start_pos and Util.hasPrefix(lx.input[lx.current_pos + 1 ..], "    ")) {
+                            _ = try lx.next();
+                            continue;
+                        }
+                        if (lx.current_pos > ls.start_pos) {
+                            try lx.emit(LexMe.Text);
+                        }
+                        lx.pos += lx.width;
+                        try lx.emit(LexMe.NewLine);
+                        break;
+                    },
+                    else => {},
+                }
+            }
+            return lex_any;
         }
     };
 
@@ -285,3 +304,125 @@ test "Lexer" {
     defer lx.deinit();
     try lx.run();
 }
+
+// Util are utility/helper functions.
+const Util = struct {
+    const punct_marks = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    fn isPunct(c: u8) bool {
+        for (punct_marks) |char| {
+            if (c == char) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// returns true if c is a whitespace character.
+    fn isSpace(c: u8) bool {
+        return isHorizontalSpace(c) or isVersicalSpace(c);
+    }
+
+    fn isBackslashEscaped(data: []const u8, i: usize) bool {
+        var bs: usize = 0;
+        while ((@intCast(isize, i) - @intCast(isize, bs) - 1) >= 0 and data[i - bs - 1] == '\\') {
+            bs += 1;
+        }
+        return bs == 1;
+    }
+
+    fn isHorizontalSpace(c: u8) bool {
+        return c == ' ' or c == '\t';
+    }
+
+    fn isVersicalSpace(c: u8) bool {
+        return c == '\n' or c == '\r' or c == form_feed or c == line_tabulation;
+    }
+
+    fn isLetter(c: u8) bool {
+        return (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z');
+    }
+
+    fn isalnum(c: u8) bool {
+        return (c >= '0' and c <= '9') or isLetter(c);
+    }
+
+    // Find if a line counts as indented or not.
+    // Returns number of characters the indent is (0 = not indented).
+    fn isIndented(data: []const u8, indent_size: usize) usize {
+        if (data.len == 0) {
+            return 0;
+        }
+        if (data[0] == '\t') {
+            return 1;
+        }
+        if (data.len < indent_size) {
+            return 0;
+        }
+        var i: usize = 0;
+        while (i < indent_size) : (i += 1) {
+            if (data[i] != space) {
+                return 0;
+            }
+        }
+        return indent_size;
+    }
+
+    /// returns true if sub_slice is within s.
+    pub fn contains(s: []const u8, sub_slice: []const u8) bool {
+        return mem.indexOf(u8, s, sub_slice) != null;
+    }
+
+    /// hasPrefix returns true if slice s begins with prefix.
+    pub fn hasPrefix(s: []const u8, prefix: []const u8) bool {
+        return s.len >= prefix.len and
+            equal(s[0..prefix.len], prefix);
+    }
+
+    pub fn hasSuffix(s: []const u8, suffix: []const u8) bool {
+        return s.len >= suffix.len and
+            equal(s[s.len - suffix.len ..], suffix);
+    }
+
+    pub fn trimPrefix(s: []const u8, prefix: []const u8) []const u8 {
+        return mem.trimLeft(u8, s, prefix);
+    }
+
+    pub fn trimSuffix(s: []const u8, suffix: []const u8) []const u8 {
+        return mem.trimRight(u8, s, suffix);
+    }
+
+    fn skipUntilChar(text: []const u8, start: usize, c: usize) usize {
+        var i = start;
+        while (i < tag.len and text[i] != c) : (i += 1) {}
+        return i;
+    }
+
+    fn skipSpace(tag: []const u8, i: usize) usize {
+        while (i < tag.len and isSpace(tag[i])) : (i += 1) {}
+        return i;
+    }
+
+    fn skipChar(data: []const u8, start: usize, c: u8) usize {
+        var i = start;
+        while (i < data.len and data[i] == c) : (i += 1) {}
+        return i;
+    }
+
+    fn isRelativeLink(link: []const u8) bool {
+        if (link[0] == '#') {
+            return true;
+        }
+        if (link.len >= 2 and link[0] == '/' and link[1] != '/') {
+            return true;
+        }
+        // current directory : begin with "./"
+        if (hasPrefix(link, "./")) {
+            return true;
+        }
+        // parent directory : begin with "../"
+        if (hasPrefix(link, "../")) {
+            return true;
+        }
+        return false;
+    }
+};
